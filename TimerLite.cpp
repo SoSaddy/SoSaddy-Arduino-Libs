63218220c9b28adbe983f2e107bb984156d95822
SoSaddy <86183342+SoSaddy@users.noreply.github.com>
2025-11-17 17:34:54 +0500

    Update TimerLite.cpp

--- TimerLite.cpp
+++ TimerLite.cpp
@@ -1,21 +1,21 @@
 #include <TimerLite.h>
 
 // Конструктор
-Timer::Timer(uint32_t cooldown) {
+TimerLite::TimerLite(uint32_t cooldown) {
 	clear();
 	this->_cooldown = cooldown;
 }
 
 // Получить состояние
-bool Timer::get() {
+bool TimerLite::get() {
 	return _getState(this->_cooldown);
 }
-bool Timer::get(uint32_t& cooldown) {
+bool TimerLite::get(uint32_t& cooldown) {
 	return _getState(cooldown);
 }
 
 // Получить состояние (приватный метод)
-bool Timer::_getState(const uint32_t& cooldown) {
+bool TimerLite::_getState(const uint32_t& cooldown) {
 	if (millis() - this->_time >= cooldown) {
 		clear();
 		return true;
@@ -24,11 +24,12 @@ bool Timer::_getState(const uint32_t& cooldown) {
 }
 
 // Установка задержки
-void Timer::setCooldown(uint32_t cooldown) {
+void TimerLite::setCooldown(uint32_t cooldown) {
 	this->_cooldown = cooldown;
 }
 
 // Обнуление задержки
 void Timer::clear() {
 	this->_time = millis();
+
 }
